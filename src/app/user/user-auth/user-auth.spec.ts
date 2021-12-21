126c8ac252d8f35267a1774168b0f329774735ad
danielasimiganoschi <daniela.simiganoschi95@gmail.com>
2021-12-21 12:30:43 +0200

    modified test files

--- src/app/user/user-auth/user-auth.spec.ts
+++ src/app/user/user-auth/user-auth.spec.ts
@@ -8,7 +8,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router } from '@angular/router';
-import { ClarityModule } from '@clr/angular';
 
 
 describe('UserAuthComponent', () => {
@@ -17,7 +16,7 @@ describe('UserAuthComponent', () => {
     let de: DebugElement;
     let el: HTMLElement;
 
-    beforeEach((done) => {
+    beforeEach(fakeAsync(() => {
         TestBed.configureTestingModule({
             declarations: [
                 UserAuthComponent
@@ -27,8 +26,7 @@ describe('UserAuthComponent', () => {
                 FormsModule,
                 ReactiveFormsModule,
                 RouterTestingModule,
-                HttpClientTestingModule,
-                ClarityModule
+                HttpClientTestingModule
             ],
             providers: [
                 JwtHelperService,
@@ -42,42 +40,39 @@ describe('UserAuthComponent', () => {
             de = fixture.debugElement.query(By.css('form'));
             el = de.nativeElement;
             fixture.detectChanges();
-            done();
         });
-    });
-    afterEach(() => {
-        fixture.destroy();
-      });
+    }));
 
 
-    it(`should set submitted to true`, () => {
+    it(`should set submitted to true`, fakeAsync(() => {
         comp.onLoginUser();
         expect(comp.isSubmitted).toBeTruthy();
-    });
+    }));
 
-    it(`should call the onLoginUser method`,() => {
+    it(`should call the onLoginUser method`, fakeAsync(() => {
         spyOn(comp, 'onLoginUser');
         comp.form.controls['username'].setValue('dsimiganoschi');
         comp.form.controls['password'].setValue('1244');
         let btn = fixture.debugElement.query(By.css('button[type=submit]')).nativeElement;
         btn.click();
+        tick();
         fixture.detectChanges();
         expect(comp.onLoginUser).toHaveBeenCalled();
-    });
+    }));
 
-    it(`form should be invalid`, () => {
+    it(`form should be invalid`, fakeAsync(() => {
         comp.form.controls['username'].setValue('');
         comp.form.controls['password'].setValue('');
 
         expect(comp.form.valid).toBeFalsy();
-    });
+    }));
 
-    it(`form should be valid`, () => {
+    it(`form should be valid`, fakeAsync(() => {
         comp.form.controls['username'].setValue('dsimiganoschi');
         comp.form.controls['password'].setValue('1244');
 
         expect(comp.form.valid).toBeTruthy();
-    });
+    }));
 
     it('should test input errors', () => {
         const username = comp.form.controls.username;
