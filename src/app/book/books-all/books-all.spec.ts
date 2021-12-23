c3b94f6a5a480639e042327bf39479a2442033ca
danielasimiganoschi <daniela.simiganoschi95@gmail.com>
2021-12-23 10:19:52 +0200

    modified  test file

--- src/app/book/books-all/books-all.spec.ts
+++ src/app/book/books-all/books-all.spec.ts
@@ -1,71 +1,66 @@
-// import { TestBed, ComponentFixture, waitForAsync, fakeAsync } from '@angular/core/testing';
-// import { BrowserModule, By } from '@angular/platform-browser';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { Router } from '@angular/router';
-// import { BooksAllComponent } from './books-all.component';
-// import { AuthorService } from 'src/app/service/author.service';
-// import { of } from 'rxjs';
-// import { BookService } from 'src/app/service/book.service';
-// import { ClarityModule, ClrIconModule } from '@clr/angular';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { CdsModule } from '@cds/angular';
-// import { CommonModule } from '@angular/common';
-// import 'globalthis/polyfill';
+import { TestBed, ComponentFixture, waitForAsync, fakeAsync } from '@angular/core/testing';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { BooksAllComponent } from './books-all.component';
+import { AuthorService } from 'src/app/service/author.service';
+import { of } from 'rxjs';
+import { BookService } from 'src/app/service/book.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import 'globalthis/polyfill';
 
-// describe('BooksAllComponent', () => {
-//     let comp: BooksAllComponent;
-//     let fixture: ComponentFixture<BooksAllComponent>;
-//     let mockBookService;
+describe('BooksAllComponent', () => {
+    let comp: BooksAllComponent;
+    let fixture: ComponentFixture<BooksAllComponent>;
+    let mockBookService;
 
-//     beforeEach(fakeAsync(() => {
+    beforeEach(fakeAsync(() => {
 
-//         mockBookService = jasmine.createSpyObj(['getBooks']);
-//         mockBookService.getBooks.and.returnValue(of([]));
+        mockBookService = jasmine.createSpyObj(['getBooks']);
+        mockBookService.getBooks.and.returnValue(of([]));
 
-//         TestBed.configureTestingModule({
-//             declarations: [
-//                 BooksAllComponent
-//             ],
-//             imports: [
-//                 CommonModule,
-//                 BrowserModule,
-//                 HttpClientTestingModule,
-//                 BrowserAnimationsModule,
-//                 ClarityModule,
-//                 CdsModule,
-//                 ClrIconModule
-//             ],
-//             providers: [
-//                 { provide: Router, useClass: class { navigate = jasmine.createSpy("navigate"); } },
-//                 { provide: AuthorService, useValue: [] },
-//                 { provide: BookService, useValue: mockBookService }
-//             ]
-//         });
-//     }));
+        TestBed.configureTestingModule({
+            declarations: [
+                BooksAllComponent
+            ],
+            imports: [
+                CommonModule,
+                BrowserModule,
+                HttpClientTestingModule,
+                BrowserAnimationsModule
+            ],
+            providers: [
+                { provide: Router, useClass: class { navigate = jasmine.createSpy("navigate"); } },
+                { provide: AuthorService, useValue: [] },
+                { provide: BookService, useValue: mockBookService }
+            ]
+        });
+    }));
 
-//     fit('should create', () => {
-//         fixture = TestBed.createComponent(BooksAllComponent);
-//         comp = fixture.componentInstance;
-//         fixture.detectChanges();
-//         expect(comp).toBeTruthy();
-//     });
+    fit('should create', () => {
+        fixture = TestBed.createComponent(BooksAllComponent);
+        comp = fixture.componentInstance;
+        fixture.detectChanges();
+        expect(comp).toBeTruthy();
+    });
 
-//     fit(`should call the getBooks method`, () => {
-//         fixture = TestBed.createComponent(BooksAllComponent);
-//         comp = fixture.componentInstance;
-//         fixture.detectChanges();
-//         expect(comp.getBooks.length).toEqual(0);
-//     });
+    fit(`should call the getBooks method`, () => {
+        fixture = TestBed.createComponent(BooksAllComponent);
+        comp = fixture.componentInstance;
+        fixture.detectChanges();
+        expect(comp.getBooks.length).toEqual(0);
+    });
 
 
-//     fit(`should show Modal`, () => {
-//         fixture = TestBed.createComponent(BooksAllComponent);
-//         comp = fixture.componentInstance;
-//         fixture.detectChanges();
+    fit(`should show Modal`, () => {
+        fixture = TestBed.createComponent(BooksAllComponent);
+        comp = fixture.componentInstance;
+        fixture.detectChanges();
 
-//         comp.deleteBook(1);
-//         fixture.detectChanges();
-//         expect(comp.isModalVisible).toBeTruthy();
-//     });
+        comp.deleteBook(1);
+        fixture.detectChanges();
+        expect(comp.isModalVisible).toBeTruthy();
+    });
 
-// });
+});
